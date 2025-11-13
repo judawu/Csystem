cb503976f21fdb2f5a19a301b78b581347ccda18
judawu <59474654+judawu@users.noreply.github.com>
2025-11-13 04:03:18 +0000

    update tcpserver1 and add executable tcpserver1

--- tcpserver1.c
+++ tcpserver1.c
@@ -1,4 +1,7 @@
-/*sockets.c */
+/*sockets.c
+netstat  -an | grep LISTEN 
+env - telnet localhost 8181
+curl -I localhost:8181 */
 #include <stdio.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,37 +11,61 @@
 #include <stdlib.h>
 
        
-#define HOST "google.com"
-#define IP "142.251.46.206" /*google.com*/
-#define PORT 80
+#define PORT 8181
 
 int main() {
-    printf("Connecting to IP: %s on PORT: %d\n", IP, PORT);
-    // Socket connection code would go here
-    int s;
-    struct sockaddr_in sock;
-    char buffer[1024] ;
+  
+    int s,c;
+    socklen_t addrlen;
+    struct sockaddr_in server,client;
+    char buffer[1024];
     char *data;
-    data= "HEAD / HTTP/1.0\r\n\r\n";
+
+    addrlen=0;
+    memset(&server,0,sizeof(server));
+    memset(&client,0,sizeof(client));
+    server.sin_family = AF_INET;
+
+
+  
     s=socket(AF_INET, SOCK_STREAM, 0);
     if(s < 0) {
         perror("Socket creation failed");
         return -1;
     }
-    sock.sin_addr.s_addr = inet_addr(IP);
-    sock.sin_port = htons(PORT);
-    sock.sin_family = AF_INET;
+    server.sin_addr.s_addr = 0; 
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT);
+    
 
-    if(connect(s, (struct sockaddr *)&sock, sizeof(sock)) !=0){
-        printf("Connection failed");
+    if(bind(s, (struct sockaddr *)&server, sizeof(server)) !=0){
+        printf("bind failed");
         close(s);
         return -1;
     };
-    printf("Connected successfully to %s (%s) on port %d\n", HOST,IP,PORT);
-    printf("Write head:\n%s\n", data); 
-    write(s, data, strlen(data));
-    read(s, buffer, sizeof(buffer)-1);
-    printf("Response:\n%s\n", buffer);  
+    
+    if(listen(s, 5 !=0)){
+        printf("listen failed");
+        close(s);
+        return -1;
+    };
+    printf("Server listening on port %d\n", PORT);
+     
+    c=accept(s, (struct sockaddr *)&client, &addrlen);
+     if(c<0){
+        printf("accept failed");
+        close(s);
+        return -1;
+    };
+    printf("Connection accepted from %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    printf("Waiting for data...\n");
+    memset(buffer,0,sizeof(buffer));    
+    read(c, buffer, sizeof(buffer)-1);
+    printf("Data received: %s\n", buffer);  
+    data="Http/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!"; 
+    write(c, data, strlen(data));
+    printf("Response sent\n");
+    close(c); 
     close(s);
     return 0;
 }
